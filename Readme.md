20c1522678c265f3ff23f43374c46bf38c2262b0
Ben McMahen <ben.mcmahen@gmail.com>
2013-10-08 13:12:17 -0600

    first commit, working demo

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# background-image-zoom
+
+  zoom a background image to full screen
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install eugenicsarchivesca/background-image-zoom
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,105 @@
+// on-click we clone the background element, position it
+// statically over-top the current element, and then we
+// programatically zoom it to the maximum dimensions allowed.
+
+var offset = require('offset');
+var classes = require('classes');
+var events = require('events');
+var css = require('css');
+var redraw = require('redraw');
+
+
+function ImageZoom(el, zoomURL){
+  if (!(this instanceof ImageZoom)) return new ImageZoom(el);
+  this.thumb = el;
+  this.viewport = {};
+  this.bind();
+  this.backgroundURL = zoomURL;
+}
+
+module.exports = ImageZoom;
+
+ImageZoom.prototype.bind = function(){
+  this.events = events(this.thumb, this);
+  this.events.bind('click', 'show');
+}
+
+ImageZoom.prototype.getDimensions = function(fn){
+  this.originalPosition = offset(this.thumb);
+  // IE doesnt have window.innerHeight.
+  // this.viewport.height = "innerHeight" in window
+  //   ? window.innerHeight
+  //   : document.documentElement.offsetHeight;
+
+  // this.viewport.width = "innerWidth" in window
+  //   ? window.innerWidth
+  //   : document.documentElement.offsetWidth;
+
+  this.originalDimensions = {
+    width: this.thumb.clientWidth,
+    height: this.thumb.clientHeight
+  };
+
+  this.src = this.thumb.getAttribute('data-zoom-url') || this.backgroundURL;
+  return this;
+}
+
+ImageZoom.prototype.createClone = function(){
+  this.clone = document.createElement('div');
+  classes(this.clone).add('image-clone');
+  this.cloneEvents = events(this.clone, this);
+  this.cloneEvents.bind('click', 'hide');
+  this.setOriginalDeminsions();
+  document.body.appendChild(this.clone);
+  return this;
+}
+
+ImageZoom.prototype.setOriginalDeminsions = function(){
+  // Ideally, we would use translate3d to animate the position of the
+  // element, instead of left + top. Consider using the below
+  // code in the future...
+  // var translateX = (o.x + (o.w / 2)) - (t.x + (t.w / 2));
+  // var translateY = (o.y + (o.h / 2)) - (t.y + (t.h / 2));
+  // var translate3d = 'translate3d('+ translateX +'px, '+ translateY +'px, 0)';
+  css(this.clone, {
+    top: this.originalPosition.top,
+    left: this.originalPosition.left,
+    width: this.originalDimensions.width,
+    height: this.originalDimensions.height,
+    'background-image' : 'url('+ this.src +')'
+  });
+  return this;
+}
+
+ImageZoom.prototype.zoomToFull = function(){
+  redraw(this.clone);
+  css(this.clone, {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%'
+  });
+
+
+  return this;
+};
+
+ImageZoom.prototype.show = function(e){
+  if (e) e.preventDefault();
+  this
+    .getDimensions()
+    .createClone()
+    .zoomToFull();
+  return this;
+}
+
+ImageZoom.prototype.hide = function(e){
+  if (e) e.preventDefault();
+  this.cloneEvents.unbind();
+  this.setOriginalDeminsions();
+  var self = this;
+  setTimeout(function(){
+    self.clone.parentNode.removeChild(self.clone);
+  }, 500);
+  return this;
+}
